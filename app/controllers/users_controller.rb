027c5378a2160158ec8bd803014b2d914e8cc46c
marina-wada <wdmrna4i1@gmail.com>
2021-06-06 23:44:50 +0000

    [Update]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,12 @@ class UsersController < ApplicationController
     redirect_to user_path(user.id)
   end
 
+  def index
+    @users = User.all
+    @user = current_user
+    @book_new = Book.new
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/about" => "homes#about"
 
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :create, :new, :update, :index]
 end
